90b80bb6120c887458c7573071d8a515956bd336
Edward Harrod <echarrod@gmail.com>
2017-09-14 19:08:40 +0100

    [SDK-72]: Added tests for ToDate on YotiAttributeValue

--- test/Yoti.Auth.Tests/YotiAttributeValue_Tests.cs
+++ test/Yoti.Auth.Tests/YotiAttributeValue_Tests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using static Yoti.Auth.YotiAttributeValue;
 
 namespace Yoti.Auth.Tests
@@ -34,6 +35,37 @@ namespace Yoti.Auth.Tests
             YotiAttributeValue yotiAttributeValue = new YotiAttributeValue(
                 TypeEnum.Date,
                 dateString);
+        }
+
+        [TestMethod]
+        public void YotiAttributeValue_ToDateValidDate_DoesntThrowException()
+        {
+            string dateString = "01/01/1985";
+            var yotiAttribueValue = new YotiAttributeValue(TypeEnum.Text, dateString);
+
+            var outputValue = yotiAttribueValue.ToDate();
+
+            Assert.IsInstanceOfType(outputValue, typeof(DateTime));
+        }
+
+        [TestMethod]
+        public void YotiAttributeValue_ToDateInvalidDate_ReturnsNull()
+        {
+            string dateString = "01/13/1985";
+            var yotiAttribueValue = new YotiAttributeValue(TypeEnum.Date, dateString);
+            var outputValue = yotiAttribueValue.ToDate();
+
+            Assert.IsNull(outputValue);
+        }
+
+        [TestMethod]
+        public void YotiAttributeValue_ToDateOnPng_ReturnsNull()
+        {
+            string dateString = "PngImageData";
+            var yotiAttribueValue = new YotiAttributeValue(TypeEnum.Png, dateString);
+            var outputValue = yotiAttribueValue.ToDate();
+
+            Assert.IsNull(outputValue);
         }
     }
 }
