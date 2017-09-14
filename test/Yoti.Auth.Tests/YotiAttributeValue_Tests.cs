218749cbdacf387c432d9c8f56906c895a7e9637
Edward Harrod <echarrod@gmail.com>
2017-09-14 18:25:33 +0100

    [SDK-72]: Added Tests for YotiAttributeValue - converting to bytes

---
+++ test/Yoti.Auth.Tests/YotiAttributeValue_Tests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Yoti.Auth.YotiAttributeValue;
+
+namespace Yoti.Auth.Tests
+{
+    [TestClass]
+    public class YotiAttributeValue_Tests
+    {
+        [TestMethod]
+        public void YotiAttributeValue_JpegData_DoesntThrowException()
+        {
+            string jpegData = "jpegData";
+
+            YotiAttributeValue yotiAttributeValue = new YotiAttributeValue(
+                TypeEnum.Jpeg,
+                jpegData);
+        }
+
+        [TestMethod]
+        public void YotiAttributeValue_PngData_DoesntThrowException()
+        {
+            string pngData = "pngData";
+
+            YotiAttributeValue yotiAttributeValue = new YotiAttributeValue(
+                TypeEnum.Png,
+                pngData);
+        }
+
+        [TestMethod]
+        public void YotiAttributeValue_DateData_DoesntThrowException()
+        {
+            string dateString = "01/01/1985";
+
+            YotiAttributeValue yotiAttributeValue = new YotiAttributeValue(
+                TypeEnum.Date,
+                dateString);
+        }
+    }
+}
