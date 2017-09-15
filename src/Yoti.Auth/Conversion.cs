af952c89a0d7b99bf84b6a1fde06e0e1b886e2d9
Edward Harrod <echarrod@gmail.com>
2017-09-15 14:19:49 +0100

    [SDK-72]: Changed BytesToUrlsafeBase64 method, as it had the conversion the wrong way around(!) for +/-. Also changed the comments for this pair of methods to be a summary.

--- src/Yoti.Auth/Conversion.cs
+++ src/Yoti.Auth/Conversion.cs
@@ -15,15 +15,15 @@ namespace Yoti.Auth
             return Convert.ToBase64String(bytes);
         }
 
-        /* UrlSafe Base64 uses '-' and '_' instead of '+' and '/' respectively so it can be passed
-         * as a url parameter without extra encoding.
-         */
-
+        /// <summary>
+        /// UrlSafe Base64 uses '-' instead of '+', and '_' instead of '/' so it
+        /// can be passed as a url parameter without extra encoding.
+        /// </summary>
         public static string BytesToUrlsafeBase64(byte[] bytes)
         {
             var base64 = BytesToBase64(bytes);
 
-            return base64.Replace("-", "+").Replace("/", "_");
+            return base64.Replace("+", "-").Replace("/", "_");
         }
 
         public static byte[] UtfToBytes(string utf8)
@@ -36,10 +36,10 @@ namespace Yoti.Auth
             return Convert.FromBase64String(base64);
         }
 
-        /* UrlSafe Base64 uses '-' and '_' instead of '+' and '/' respectively so it can be passed
-         * as a url parameter without extra encoding.
-         */
-
+        /// <summary>
+        /// UrlSafe Base64 uses '-' instead of '+', and '_' instead of '/' so it
+        /// can be passed as a url parameter without extra encoding.
+        /// </summary>
         public static byte[] UrlSafeBase64ToBytes(string urlSafeBase64)
         {
             var base64 = urlSafeBase64.Replace("-", "+").Replace("_", "/");
