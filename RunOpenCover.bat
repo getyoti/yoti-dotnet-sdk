9542f3449e67ff2a341b6032dfbb2e1ee7bac527
Edward Harrod <echarrod@users.noreply.github.com>
2017-09-20 14:17:21 +0100

    [SDK-93]: Made batch files more user-friendly

--- RunOpenCover.bat
+++ RunOpenCover.bat
@@ -5,8 +5,12 @@ for /R "%~dp0packages" %%a in (*) do if /I "%%~nxa"=="OpenCover.Console.exe" SET
 
 SET ProtoBufAttributeNamespace=AttrpubapiV1
 SET ProtoBufCommonNamespace=CompubapiV1
+SET OpenCoverDirectory="..\OpenCover"
+
+if not exist %OpenCoverDirectory% mkdir %OpenCoverDirectory%
 
 SET Filter="+[Yoti.Auth]* +[Yoti.Auth.Owin]* -[Yoti.Auth.Tests]* -[Yoti.Auth]%ProtoBufAttributeNamespace%* -[Yoti.Auth]%ProtoBufCommonNamespace%*"
-SET OutputFile=..\..\..\OpenCover\Yoti.Auth.Tests\coverage.xml
+SET OutputFile=%OpenCoverDirectory%\Yoti.Auth.Tests\coverage.xml
+SET TargetArgs="test test/Yoti.Auth.Tests"
 
-%OpenCoverExe% "-target:%ProgramFiles%\dotnet\dotnet.exe" -targetargs:test -register:user -filter:%Filter% -output:%OutputFile% -oldStyle
+%OpenCoverExe% "-target:%ProgramFiles%\dotnet\dotnet.exe" -targetargs:%TargetArgs% -register:user -filter:%Filter% -output:%OutputFile% -oldStyle
