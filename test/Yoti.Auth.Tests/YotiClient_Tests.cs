e7e59f057af7afb0629b80477c8ae7cb55edb1e1
Edward Harrod <echarrod@users.noreply.github.com>
2017-09-22 11:44:12 +0100

    [SDK-93]: Added tests to cover retrieving of ActivityDetails in YotiClient

--- test/Yoti.Auth.Tests/YotiClient_Tests.cs
+++ test/Yoti.Auth.Tests/YotiClient_Tests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Yoti.Auth.Tests
@@ -7,6 +8,8 @@ namespace Yoti.Auth.Tests
     [TestClass]
     public class YotiClient_Tests
     {
+        private const string encryptedToken = "b6H19bUCJhwh6WqQX/sEHWX9RP+A/ANr1fkApwA4Dp2nJQFAjrF9e6YCXhNBpAIhfHnN0iXubyXxXZMNwNMSQ5VOxkqiytrvPykfKQWHC6ypSbfy0ex8ihndaAXG5FUF+qcU8QaFPMy6iF3x0cxnY0Ij0kZj0Ng2t6oiNafb7AhT+VGXxbFbtZu1QF744PpWMuH0LVyBsAa5N5GJw2AyBrnOh67fWMFDKTJRziP5qCW2k4h5vJfiYr/EOiWKCB1d/zINmUm94ZffGXxcDAkq+KxhN1ZuNhGlJ2fKcFh7KxV0BqlUWPsIEiwS0r9CJ2o1VLbEs2U/hCEXaqseEV7L29EnNIinEPVbL4WR7vkF6zQCbK/cehlk2Qwda+VIATqupRO5grKZN78R9lBitvgilDaoE7JB/VFcPoljGQ48kX0wje1mviX4oJHhuO8GdFITS5LTbojGVQWT7LUNgAUe0W0j+FLHYYck3v84OhWTqads5/jmnnLkp9bdJSRuJF0e8pNdePnn2lgF+GIcyW/0kyGVqeXZrIoxnObLpF+YeUteRBKTkSGFcy7a/V/DLiJMPmH8UXDLOyv8TVt3ppzqpyUrLN2JVMbL5wZ4oriL2INEQKvw/boDJjZDGeRlu5m1y7vGDNBRDo64+uQM9fRUULPw+YkABNwC0DeShswzT00=";
+
         private StreamReader GetValidKeyStream()
         {
             return File.OpenText("test-key.pem");
@@ -67,6 +70,32 @@ namespace Yoti.Auth.Tests
             {
                 YotiClient client = new YotiClient(sdkId, keystream);
             });
+        }
+
+        [TestMethod]
+        public void YotiClient_GetActivityDetails()
+        {
+            string sdkId = "fake-sdk-id";
+            var privateStreamKey = GetValidKeyStream();
+
+            YotiClient client = new YotiClient(sdkId, privateStreamKey);
+
+            ActivityDetails activityDetails = client.GetActivityDetails(encryptedToken);
+
+            Assert.IsNotNull(activityDetails.Outcome);
+        }
+
+        [TestMethod]
+        public async Task YotiClient_GetActivityDetailsAsync()
+        {
+            string sdkId = "fake-sdk-id";
+            var privateStreamKey = GetValidKeyStream();
+
+            YotiClient client = new YotiClient(sdkId, privateStreamKey);
+
+            ActivityDetails activityDetails = await client.GetActivityDetailsAsync(encryptedToken);
+
+            Assert.IsNotNull(activityDetails.Outcome);
         }
     }
 }
