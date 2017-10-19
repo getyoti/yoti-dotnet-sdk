ce890dd82dd8c4def37fd8908e806f0f73d2c405
Edward Harrod <echarrod@users.noreply.github.com>
2017-10-19 17:08:08 +0100

    [SDK-137]: Added Base64Data Helper

--- src/Yoti.Auth/YotiClientEngine.cs
+++ src/Yoti.Auth/YotiClientEngine.cs
@@ -146,7 +146,8 @@ namespace Yoti.Auth
                                 profile.Selfie = new Image
                                 {
                                     Type = ImageType.Jpeg,
-                                    Data = data
+                                    Data = data,
+                                    Base64Data = Convert.ToBase64String(data)
                                 };
                                 break;
 
@@ -154,7 +155,8 @@ namespace Yoti.Auth
                                 profile.Selfie = new Image
                                 {
                                     Type = ImageType.Png,
-                                    Data = data
+                                    Data = data,
+                                    Base64Data = Convert.ToBase64String(data)
                                 };
                                 break;
                         }
