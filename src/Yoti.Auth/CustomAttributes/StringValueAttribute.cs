9238eb08b113a795d2e97269ebcb6b88dd7b05d2
Edward Harrod <echarrod@users.noreply.github.com>
2018-04-20 16:13:37 +0100

    [SDK-331]: Added Extension method for setting property, and custom attributes for AnchorType class

---
+++ src/Yoti.Auth/CustomAttributes/StringValueAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Yoti.Auth
+{
+    internal class StringValueAttribute : System.Attribute
+    {
+        public readonly string StringValue;
+
+        public StringValueAttribute(string stringValue)
+        {
+            StringValue = stringValue;
+        }
+    }
+}
---
+++ src/Yoti.Auth/ExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Yoti.Auth
+{
+    public static class ExtensionMethods
+    {
+        public static void SetPropertyValue<T, TValue>(this T target, Expression<Func<T, TValue>> memberLamda, TValue value)
+        {
+            if (memberLamda.Body is MemberExpression memberSelectorExpression)
+            {
+                if (memberSelectorExpression.Member is PropertyInfo property)
+                {
+                    property.SetValue(target, value, null);
+                }
+            }
+        }
+    }
+}
