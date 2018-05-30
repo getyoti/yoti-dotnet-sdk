bd5472fdf827413faa673b4cd3af7f3656dbd732
Edward Harrod <echarrod@users.noreply.github.com>
2018-05-30 14:09:54 +0100

    SDK-331: Added object summaries

--- src/Yoti.Auth/Anchors/Anchor.cs
+++ src/Yoti.Auth/Anchors/Anchor.cs
@@ -6,6 +6,14 @@ using Google.Protobuf.Collections;
 
 namespace Yoti.Auth.Anchors
 {
+    /// <summary>
+    /// <para>A class to represent a Yoti anchor. Anchors are metadata associated
+    /// to the attribute, which describe how an attribute has been provided
+    /// to Yoti (SOURCE Anchor) and how it has been verified (VERIFIER Anchor)</para>
+    /// <para>If an attribute has only one SOURCE Anchor with the value set to
+    /// "USER_PROVIDED" and zero VERIFIER Anchors, then the attribute
+    /// is a self-certified one</para>
+    /// </summary>
     public class Anchor
     {
         private readonly AnchorType _anchorType;
@@ -43,41 +51,99 @@ namespace Yoti.Auth.Anchors
             return X509originServerCerts;
         }
 
+        /// <summary>
+        /// Gets the <see cref="AnchorType"/> of the given anchor
+        /// </summary>
+        /// <returns>The <see cref="AnchorType"/> of the given anchor</returns>
         public AnchorType GetAnchorType()
         {
             return _anchorType;
         }
 
+        /// <summary>
+        /// Gets the value of the given anchor.
+        /// <para>For <see cref="AnchorType.Source"/> this can be "USER_PROVIDED", "PASSPORT",
+        /// "DRIVING_LICENCE" or "AADHAAR"</para>
+        /// <para>For <see cref="AnchorType.Verifier"/> this
+        /// can be "YOTI_ADMIN", "YOTI_IDENTITY", "YOTI_OTP", "YOTI_UIDAI" or "PASSPORT_NFC_SIGNATURE"</para>
+        /// </summary>
+        /// <returns>A list of strings of the value(s) for this anchor type</returns>
         public List<string> GetValue()
         {
             return _value;
         }
 
+        /// <summary>
+        /// ArtifactLink gives the database ID of the Yoti stored copy of the backing artifact/document.
+        /// </summary>
+        /// <returns>The database ID as a byte array</returns>
         public byte[] GetArtifactLink()
         {
             return _artifactLink;
         }
 
+        /// <summary>
+        /// ArtifactSignature is a signature over the backing artifact. When a
+        /// client validates the Signature field, the data being signed
+        /// encompasses this signature. When the Yoti server validates the
+        /// Signature field, it can further verify that this signature matches
+        /// the one in the source artifact itself.
+        /// </summary>
+        /// <returns>An artifact signature as a byte array</returns>
         public byte[] GetArtifactSignature()
         {
             return _artifactSignature;
         }
 
+        /// <summary>
+        /// <para>OriginServerCerts are the X.509 certificate chain(DER-encoded ASN.1)
+        /// from the service that assigned the attribute</para>
+        ///
+        /// <para>The first certificate in the chain holds the public key that can be
+        /// used to verify the Signature field; any following entries (zero or
+        /// more) are for intermediate certificate authorities (in order). The
+        /// last certificate in the chain must be verified against the Yoti root
+        /// CA certificate</para>
+        ///
+        /// <para>An extension in the first certificate holds the main artifact type,
+        /// e.g. “PASSPORT”, which can alternatively be retrieved with <see cref="GetValue()"/></para>
+        /// </summary>
+        /// <returns>The X509 certificate chain from the service that assigned the attribute</returns>
         public List<X509Certificate2> GetOriginServerCerts()
         {
             return _originServerCerts;
         }
 
+        /// <summary>
+        /// Signature is a marshaled signature message which
+        /// contains an RSA signature (currently SHA512WithRSA) along with the
+        /// hash+sign algorithm details. The signature is over a subset of fields
+        /// in the <see cref="YotiAttribute{T}"/>.
+        /// </summary>
+        /// <returns>Marshaled signature message in a byte array</returns>
         public byte[] GetSignature()
         {
             return _signature;
         }
 
+        /// <summary>
+        /// SignedTimeStamp is the time at which the signature was created. The
+        /// message associated with the timestamp is the marshaled form of
+        /// AttributeSigning (i.e. the same message that is signed in the
+        /// Signature field).
+        /// </summary>
+        /// <returns>The signed timestamp in a byte array</returns>
         public byte[] GetSignedTimeStamp()
         {
             return _signedTimeStamp;
         }
 
+        /// <summary>
+        /// SubType is an indicator of any specific processing method, or
+        /// subcategory, pertaining to an artifact. For example, for a passport, this would be
+        /// either "NFC" or "OCR".
+        /// </summary>
+        /// <returns>The subtype of an artifact</returns>
         public string GetSubType()
         {
             return _subType;
--- src/Yoti.Auth/YotiAttribute.cs
+++ src/Yoti.Auth/YotiAttribute.cs
@@ -5,6 +5,12 @@ using Yoti.Auth.Anchors;
 
 namespace Yoti.Auth
 {
+    /// <summary>
+    /// A class to represent a Yoti attribute. A Yoti attribute consists of the attribute name, an associated
+    /// <see cref="YotiAttributeValue"/>, and a list of <see cref="Anchor"/>s from Yoti.
+    /// It may hold one or more anchors, which specify how an attribute has been provided
+    /// and how it has been verified within the Yoti platform.
+    /// </summary>
     public class YotiAttribute<T>
     {
         protected readonly YotiAttributeValue Value;
@@ -24,11 +30,19 @@ namespace Yoti.Auth
             _anchors = anchors;
         }
 
+        /// <summary>
+        /// Gets the name of the attribute
+        /// </summary>
+        /// <returns>Attribute name</returns>
         public string GetName()
         {
             return _name;
         }
 
+        /// <summary>
+        /// Retrieves the value of an attribute. If this is null, the default value for the type is returned.
+        /// </summary>
+        /// <returns>Value of the attribute</returns>
         public T GetValue()
         {
             if (Value == null)
@@ -37,11 +51,20 @@ namespace Yoti.Auth
             return Value.ToBytes().ConvertType<T>();
         }
 
+        /// <summary>
+        /// Gets the json value of an attribute, in the form of a <see cref="Dictionary{string, JToken}"/>
+        /// </summary>
+        /// <returns>JSON value of an attribute</returns>
         public Dictionary<string, JToken> GetJsonValue()
         {
             return Value.ToJson();
         }
 
+        /// <summary>
+        /// Attempts to get the value of the attribute, and if this is null, then returns the specified default value
+        /// </summary>
+        /// <param name="defaultValue"></param>
+        /// <returns>The value of the attribute, or if this is null, the default value</returns>
         public T GetValueOrDefault(T defaultValue)
         {
             T value = GetValue();
@@ -52,16 +75,32 @@ namespace Yoti.Auth
             return defaultValue;
         }
 
+        /// <summary>
+        /// Get the anchors for an attribute. If an attribute has only one SOURCE
+        /// Anchor with the value set to "USER_PROVIDED" and zero VERIFIER Anchors,
+        /// then the attribute is a self-certified one.
+        /// </summary>
+        /// <returns>A list of all of the anchors associated with an attribute</returns>
         public List<Anchor> GetAnchors()
         {
             return _anchors.ToList();
         }
 
+        /// <summary>
+        /// Sources are a subset of the anchors associated with an attribute, where the
+        /// anchor type is <see cref="AnchorType.Source"/>".
+        /// </summary>
+        /// <returns>A list of <see cref="AnchorType.Source"/>" anchors</returns>
         public List<Anchor> GetSources()
         {
             return _anchors.Where(a => a.GetAnchorType() == AnchorType.Source).ToList();
         }
 
+        /// <summary>
+        /// Verifiers are a subset of the anchors associated with an attribute, where the
+        /// anchor type is <see cref="AnchorType.Verifier"/>".
+        /// </summary>
+        /// <returns>A list of <see cref="AnchorType.Verifier"/>" anchors</returns>
         public List<Anchor> GetVerifiers()
         {
             return _anchors.Where(a => a.GetAnchorType() == AnchorType.Verifier).ToList();
