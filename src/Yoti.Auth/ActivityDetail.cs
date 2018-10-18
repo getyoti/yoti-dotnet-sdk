6c3bb0e7e2a0d6f46d6074aa6d0cc249d1a745d3
echarrod <echarrod@users.noreply.github.com>
2018-10-18 12:25:52 +0100

    [SDK-554] Add ReceiptID to ActivityDetails

--- src/Yoti.Auth/ActivityDetail.cs
+++ src/Yoti.Auth/ActivityDetail.cs
@@ -12,20 +12,42 @@ namespace Yoti.Auth
     /// </summary>
     public class ActivityDetails
     {
+        public ActivityDetails(ActivityOutcome activityOutcome)
+        {
+            UserProfile = null;
+            Profile = null;
+            ReceiptID = null;
+            Outcome = activityOutcome;
+        }
+
+        public ActivityDetails(YotiUserProfile yotiUserProfile, YotiProfile yotiProfile, string receiptID, ActivityOutcome activityOutcome)
+        {
+            UserProfile = yotiUserProfile;
+            Profile = yotiProfile;
+            ReceiptID = receiptID;
+            Outcome = activityOutcome;
+        }
+
         /// <summary>
         /// The <see cref="YotiUserProfile"/> returned by Yoti if the request was successful.
         /// </summary>
         [Obsolete("Please use Profile instead")]
-        public YotiUserProfile UserProfile { get; set; }
+        public YotiUserProfile UserProfile { get; private set; }
 
         /// <summary>
         /// The <see cref="YotiProfile"/> returned by Yoti if the request was successful.
         /// </summary>
-        public YotiProfile Profile { get; set; }
+        public YotiProfile Profile { get; private set; }
 
         /// <summary>
         /// The outcome status of the request.
         /// </summary>
-        public ActivityOutcome Outcome { get; set; }
+        public ActivityOutcome Outcome { get; private set; }
+
+        /// <summary>
+        /// Receipt ID identifying a completed activity.
+        /// </summary>
+        /// <returns></returns>
+        public string ReceiptID { get; private set; }
     }
 }
