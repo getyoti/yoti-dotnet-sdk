7d8f751eab2602b89a74dbb69aad0778120183e3
echarrod <echarrod@users.noreply.github.com>
2018-10-30 17:30:22 +0000

    SDK-587: Restructure profile attributes to be in a dictionary

--- src/Yoti.Auth/AttributeConverter.cs
+++ src/Yoti.Auth/AttributeConverter.cs
@@ -9,47 +9,6 @@ namespace Yoti.Auth
 {
     internal class AttributeConverter
     {
-        [Obsolete("Will be using ConvertToBaseAttribute instead")]
-        public static YotiAttribute<object> ConvertAttribute(AttrpubapiV1.Attribute attribute)
-        {
-            YotiAttributeValue value;
-
-            switch (attribute.ContentType)
-            {
-                case ContentType.String:
-                    value = new YotiAttributeValue(TypeEnum.Text, attribute.Value.ToByteArray());
-                    break;
-
-                case ContentType.Date:
-                    value = new YotiAttributeValue(TypeEnum.Date, attribute.Value.ToByteArray());
-                    break;
-
-                case ContentType.Jpeg:
-                    value = new YotiAttributeValue(TypeEnum.Jpeg, attribute.Value.ToByteArray());
-                    break;
-
-                case ContentType.Png:
-                    value = new YotiAttributeValue(TypeEnum.Png, attribute.Value.ToByteArray());
-                    break;
-
-                case ContentType.Json:
-                    value = new YotiAttributeValue(TypeEnum.Json, attribute.Value.ToByteArray());
-                    break;
-
-                case ContentType.Undefined:
-                    // do not return attributes with undefined content types
-                    return null;
-
-                default:
-                    return null;
-            }
-
-            return new YotiAttribute<object>(
-                        attribute.Name,
-                        value,
-                        ParseAnchors(attribute));
-        }
-
         public static BaseAttribute ConvertToBaseAttribute(AttrpubapiV1.Attribute attribute)
         {
             YotiAttributeValue value;
--- src/Yoti.Auth/YotiProfile.cs
+++ src/Yoti.Auth/YotiProfile.cs
@@ -5,8 +5,16 @@ using Yoti.Auth.CustomAttributes;
 
 namespace Yoti.Auth
 {
-    public class YotiProfile
+    public class YotiProfile : Profile
     {
+        internal YotiProfile() : base()
+        {
+        }
+
+        internal YotiProfile(Dictionary<string, BaseAttribute> attributes) : base(attributes)
+        {
+        }
+
         /// <summary>
         /// The unique identifier returned by Yoti.
         /// </summary>
@@ -16,77 +24,128 @@ namespace Yoti.Auth
         /// <summary>
         /// Selfie is a photograph of the user. This will be null if not provided by Yoti
         /// </summary>
-        [ProtobufName("selfie")]
-        public YotiImageAttribute<Image> Selfie { get; set; }
+        public YotiAttribute<Image> Selfie
+        {
+            get
+            {
+                return GetAttributeByName<Image>(name: Constants.UserProfile.SelfieAttribute);
+            }
+        }
 
         /// <summary>
         /// FullName represents the user's full name. This will be null if not provided by Yoti
         /// </summary>
-        [ProtobufName("full_name")]
-        public YotiAttribute<string> FullName { get; set; }
+        public YotiAttribute<string> FullName
+        {
+            get
+            {
+                return GetAttributeByName<string>(name: Constants.UserProfile.FullNameAttribute);
+            }
+        }
 
         /// <summary>
         /// GivenNames represents the user's given names. This will be null if not provided by Yoti
         /// </summary>
-        [ProtobufName("given_names")]
-        public YotiAttribute<string> GivenNames { get; set; }
+        public YotiAttribute<string> GivenNames
+        {
+            get
+            {
+                return GetAttributeByName<string>(name: Constants.UserProfile.GivenNamesAttribute);
+            }
+        }
 
         /// <summary>
         /// FamilyName represents the user's family name. This will be null if not provided by Yoti
         /// </summary>
-        [ProtobufName("family_name")]
-        public YotiAttribute<string> FamilyName { get; set; }
+        public YotiAttribute<string> FamilyName
+        {
+            get
+            {
+                return GetAttributeByName<string>(name: Constants.UserProfile.FamilyNameAttribute);
+            }
+        }
 
         /// <summary>
         /// MobileNumber represents the user's mobile phone number. This will be null if not provided by Yoti
         /// </summary>
-        [ProtobufName("phone_number")]
-        public YotiAttribute<string> MobileNumber { get; set; }
+        public YotiAttribute<string> MobileNumber
+        {
+            get
+            {
+                return GetAttributeByName<string>(name: Constants.UserProfile.PhoneNumberAttribute);
+            }
+        }
 
         /// <summary>
         /// EmailAddress represents the user's email address. This will be null if not provided by Yoti
         /// </summary>
-        [ProtobufName("email_address")]
-        public YotiAttribute<string> EmailAddress { get; set; }
+        public YotiAttribute<string> EmailAddress
+        {
+            get
+            {
+                return GetAttributeByName<string>(name: Constants.UserProfile.EmailAddressAttribute);
+            }
+        }
 
         /// <summary>
         /// DateOfBirth represents the user's date of birth. This will be null if not provided by Yoti
         /// </summary>
-        [ProtobufName("date_of_birth")]
-        public YotiAttribute<DateTime?> DateOfBirth { get; set; }
-
-        /// <summary>
-        /// AgeVerified Did the user pass the age verification check? Returns True if they passed, False if they failed, and null if there was no check
-        /// </summary>
-        public YotiAttribute<bool?> AgeVerified { get; set; }
+        public YotiAttribute<DateTime?> DateOfBirth
+        {
+            get
+            {
+                return GetAttributeByName<DateTime?>(name: Constants.UserProfile.DateOfBirthAttribute);
+            }
+        }
 
         /// <summary>
         /// Address represents the user's address. This will be null if not provided by Yoti
         /// </summary>
-        [ProtobufName("postal_address")]
-        public YotiAttribute<string> Address { get; set; }
+        public YotiAttribute<string> Address
+        {
+            get
+            {
+                return GetAttributeByName<string>(name: Constants.UserProfile.PostalAddressAttribute);
+            }
+        }
 
         /// <summary>
         /// StructuredPostalAddress represents the user's address represented as a dictionary. This will be null if not provided by Yoti
         /// </summary>
-        [ProtobufName("structured_postal_address")]
-        public YotiAttribute<IEnumerable<Dictionary<string, JToken>>> StructuredPostalAddress { get; set; }
+        public YotiAttribute<IEnumerable<Dictionary<string, JToken>>> StructuredPostalAddress
+        {
+            get
+            {
+                return GetAttributeByName<IEnumerable<Dictionary<string, JToken>>>(name: Constants.UserProfile.StructuredPostalAddressAttribute);
+            }
+        }
 
         /// <summary>
         /// Gender represents the user's gender. This will be null if not provided by Yoti
         /// </summary>
-        [ProtobufName("gender")]
-        public YotiAttribute<string> Gender { get; set; }
+        public YotiAttribute<string> Gender
+        {
+            get
+            {
+                return GetAttributeByName<string>(name: Constants.UserProfile.GenderAttribute);
+            }
+        }
 
         /// <summary>
         /// Nationality represents the user's nationality. This will be null if not provided by Yoti
         /// </summary>
-        [ProtobufName("nationality")]
-        public YotiAttribute<string> Nationality { get; set; }
+        public YotiAttribute<string> Nationality
+        {
+            get
+            {
+                return GetAttributeByName<string>(name: Constants.UserProfile.NationalityAttribute);
+            }
+        }
 
         /// <summary>
         /// Other profile data returned by Yoti.
         /// </summary>
+        [Obsolete("Not needed, use GetAttributeByName instead")]
         public Dictionary<string, YotiAttributeValue> OtherAttributes { get; set; }
     }
 }
