c057d842c39620f50ed839f70f6b77b59390e3da
echarrod <echarrod@users.noreply.github.com>
2018-12-14 15:21:25 +0000

    [SDK-575]: Add AgeVerifications

--- src/Yoti.Auth/BaseProfile.cs
+++ src/Yoti.Auth/BaseProfile.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Yoti.Auth
 {
     public abstract class BaseProfile
     {
-        private Dictionary<string, BaseAttribute> _attributes;
+        private readonly Dictionary<string, BaseAttribute> _attributes;
 
         internal BaseProfile()
         {
@@ -35,6 +36,36 @@ namespace Yoti.Auth
             }
 
             return null;
+        }
+
+        /// <summary>
+        /// Retrieves an attribute based on its name, without specifying the type
+
+        /// <summary>
+        /// Returns all of the <see cref="YotiAttribute"/>s which have a name starting with
+        /// the specified string, which can be cast to the specified type.
+        /// Returns null if there were no matches.
+        /// </summary>
+        /// <typeparam name="T">The type parameter indicating the type of the desired attribute</typeparam>
+        /// <param name="prefix">Attribute name to search for</param>
+        /// <returns>All matching attributes, null if there was no match</returns>
+        public List<YotiAttribute<T>> FindAttributesStartingWith<T>(string prefix)
+        {
+            if (prefix == null)
+                throw new ArgumentNullException(nameof(prefix));
+
+            List<YotiAttribute<T>> matches = new List<YotiAttribute<T>>();
+
+            foreach (KeyValuePair<string, BaseAttribute> attribute in _attributes)
+            {
+                if (attribute.Key.StartsWith(prefix))
+                {
+                    if (attribute.Value is YotiAttribute<T> castableAttribute)
+                        matches.Add(castableAttribute);
+                }
+            }
+
+            return matches;
         }
     }
 }
