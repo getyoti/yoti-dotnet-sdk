1b45dc686291f5efa33987b7a7555c87548c98e3
echarrod <echarrod@users.noreply.github.com>
2019-01-22 11:13:58 +0000

    SDK-497: Remove "ActivityDetails.Outcome" conditionals from Example projects

--- src/Examples/45Example/Controllers/AccountController.cs
+++ src/Examples/45Example/Controllers/AccountController.cs
@@ -43,44 +43,37 @@ namespace Example.Controllers
                 var yotiClient = new YotiClient(sdkId, privateKeyStream);
 
                 var activityDetails = yotiClient.GetActivityDetails(token);
-                if (activityDetails.Outcome == ActivityOutcome.Success)
+                var profile = activityDetails.Profile;
+
+                var user = new User
                 {
-                    var profile = activityDetails.Profile;
+                    RememberMeID = activityDetails.RememberMeId
+                };
 
-                    var user = new User
-                    {
-                        RememberMeID = activityDetails.RememberMeId
-                    };
-
-                    if (profile.Selfie != null)
-                    {
-                        user.Base64Photo = profile.Selfie.GetValue().GetBase64URI();
-                        user.Photo = profile.Selfie.GetValue().GetContent();
-                        SetPhotoBytes(user.Photo);
-                    }
-                    else
-                    {
-                        ViewBag.Message = "No photo provided, change the application settings to request a photo from the user for this demo";
-                    }
-
-                    UpdateAttributesIfPresent(profile, user);
-
-                    var identity = new ClaimsIdentity(new[] {
-                        new Claim(ClaimTypes.Name, user.RememberMeID.ToString()),
-                        },
-                        "ApplicationCookie");
-
-                    var ctx = Request.GetOwinContext();
-                    var authManager = ctx.Authentication;
-
-                    authManager.SignIn(identity);
-
-                    return View(user);
+                if (profile.Selfie != null)
+                {
+                    user.Base64Photo = profile.Selfie.GetValue().GetBase64URI();
+                    user.Photo = profile.Selfie.GetValue().GetContent();
+                    SetPhotoBytes(user.Photo);
                 }
                 else
                 {
-                    return RedirectToAction("LoginFailure", "Home");
+                    ViewBag.Message = "No photo provided, change the application settings to request a photo from the user for this demo";
                 }
+
+                UpdateAttributesIfPresent(profile, user);
+
+                var identity = new ClaimsIdentity(new[] {
+                        new Claim(ClaimTypes.Name, user.RememberMeID.ToString()),
+                        },
+                    "ApplicationCookie");
+
+                var ctx = Request.GetOwinContext();
+                var authManager = ctx.Authentication;
+
+                authManager.SignIn(identity);
+
+                return View(user);
             }
             catch (Exception e)
             {
