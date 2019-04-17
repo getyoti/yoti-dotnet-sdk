dfa5756e79a9db8508cd9efe293c103927dbe9e2
echarrod <echarrod@users.noreply.github.com>
2019-04-17 15:26:03 +0100

    SDK-721: Make CreateExceptionFromStatusCode generic for reuse, move exceptions to own folder

---
+++ src/Yoti.Auth/Exceptions/YotiProfileException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Yoti.Auth.Exceptions
+{
+    public class YotiProfileException : Exception
+    {
+        public YotiProfileException(string message) : base(message)
+        {
+        }
+
+        public YotiProfileException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
--- test/Yoti.Auth.Tests/YotiClientEngineTests.cs
+++ test/Yoti.Auth.Tests/YotiClientEngineTests.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Org.BouncyCastle.Crypto;
 using Yoti.Auth.Aml;
+using Yoti.Auth.Exceptions;
 using Yoti.Auth.Tests.TestTools;
 
 namespace Yoti.Auth.Tests
