cfca48d0cc000c3ca9d186d67b71b1ea859f5cab
echarrod <echarrod@users.noreply.github.com>
2019-04-30 11:56:06 +0100

    SDK-721: Update HeadersFactory logic for null arguments

---
+++ test/Yoti.Auth.Tests/SignedMessageFactoryTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yoti.Auth.Tests.TestTools;
+
+namespace Yoti.Auth.Tests
+{
+    [TestClass]
+    public class SignedMessageFactoryTests
+    {
+        [TestMethod]
+        public void ShouldThrowExceptionWhenHttpMethodIsNull()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() =>
+            {
+                SignedMessageFactory.SignMessage(null, "endpoint", KeyPair.Get(), null);
+            });
+        }
+    }
+}
