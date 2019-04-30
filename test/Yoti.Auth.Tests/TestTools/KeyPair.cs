ea0572cdc5f51131c8b03e54f26b3404af72bb49
echarrod <echarrod@users.noreply.github.com>
2019-04-30 12:13:27 +0100

    SDK-721: Add keypair test tools file

---
+++ test/Yoti.Auth.Tests/TestTools/KeyPair.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using Org.BouncyCastle.Crypto;
+
+namespace Yoti.Auth.Tests.TestTools
+{
+    internal static class KeyPair
+    {
+        internal static AsymmetricCipherKeyPair Get()
+        {
+            using (StreamReader stream = File.OpenText("test-key.pem"))
+            {
+                return CryptoEngine.LoadRsaKey(stream);
+            }
+        }
+    }
+}
