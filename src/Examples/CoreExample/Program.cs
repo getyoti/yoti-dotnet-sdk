f46c1215498b19b12980d01811ea7f4f853485d1
echarrod <echarrod@users.noreply.github.com>
2019-05-08 18:43:22 +0100

    SDK-477: Add docker compatibility for CoreExample project

--- src/Examples/CoreExample/Program.cs
+++ src/Examples/CoreExample/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.IO;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace CoreExample
@@ -8,14 +7,11 @@ namespace CoreExample
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseStartup<Startup>()
-                .Build();
+                .UseStartup<Startup>();
     }
 }
