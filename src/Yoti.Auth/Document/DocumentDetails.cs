563ef37e50b5b023c4207622f4cf80437c869227
echarrod <echarrod@users.noreply.github.com>
2019-05-16 16:28:39 +0100

    SDK-694: Add to descriptions of DateTime objects to explain the times will default to 00:00:00 & complete DocumentDetails descriptions

--- src/Yoti.Auth/Document/DocumentDetails.cs
+++ src/Yoti.Auth/Document/DocumentDetails.cs
@@ -5,10 +5,31 @@ namespace Yoti.Auth.Document
 {
     public class DocumentDetails
     {
+        /// <summary>
+        /// 3 digit country code, e.g. “GBR“
+        /// </summary>
         public string IssuingCountry { get; private set; }
+
+        /// <summary>
+        /// Document number (may include letters) from the document.
+        /// </summary>
         public string DocumentNumber { get; private set; }
+
+        /// <summary>
+        /// Expiration date of the document in DateTime format.
+        /// If the document does not expire, this field will not be present.
+        /// The time part of this DateTime will default to 00:00:00.
+        /// </summary>
         public DateTime? ExpirationDate { get; private set; }
+
+        /// <summary>
+        ///  Type of the document e.g. PASSPORT | DRIVING_LICENCE | NATIONAL_ID | PASS_CARD
+        /// </summary>
         public string DocumentType { get; private set; }
+
+        /// <summary>
+        /// Can either be a country code (for a state), or the name of the issuing authority.
+        /// </summary>
         public string IssuingAuthority { get; private set; }
 
         public DocumentDetails(string documentType, string issuingCountry, string documentNumber, DateTime? expirationDate, string issuingAuthority)
