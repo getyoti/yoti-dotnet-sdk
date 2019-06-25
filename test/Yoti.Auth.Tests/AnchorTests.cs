0da62e5debb467bcff915cd7fc02446d70dae280
echarrod <echarrod@users.noreply.github.com>
2019-06-25 11:38:52 +0100

    SDK-1025: Add further anchor test coverage

--- test/Yoti.Auth.Tests/AnchorTests.cs
+++ test/Yoti.Auth.Tests/AnchorTests.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yoti.Auth.Anchors;
 using Yoti.Auth.Tests.TestData;
 using Yoti.Auth.Tests.TestTools;
 
@@ -21,17 +24,24 @@ namespace Yoti.Auth.Tests
 
             var yotiAnchor = new Yoti.Auth.Anchors.Anchor(protobufAnchor);
 
-            Assert.IsFalse(yotiAnchor.GetAnchorType().IsDefault());
+            Assert.AreEqual(AnchorType.SOURCE, yotiAnchor.GetAnchorType());
             Assert.IsFalse(yotiAnchor.GetSignature().IsDefault());
-            Assert.IsFalse(yotiAnchor.GetSignedTimeStamp().GetTimestamp().IsDefault());
-            Assert.IsFalse(yotiAnchor.GetSignedTimeStamp().GetVersion().IsDefault());
+            Assert.AreEqual(636590455839235370, yotiAnchor.GetSignedTimeStamp().GetTimestamp().Ticks);
+            Assert.AreEqual(1, yotiAnchor.GetSignedTimeStamp().GetVersion());
             Assert.IsFalse(yotiAnchor.GetSignedTimeStamp().GetMessageDigest().IsDefault());
             Assert.IsFalse(yotiAnchor.GetSignedTimeStamp().GetChainDigest().IsDefault());
             Assert.IsFalse(yotiAnchor.GetSignedTimeStamp().GetChainDigestSkip1().IsDefault());
             Assert.IsFalse(yotiAnchor.GetSignedTimeStamp().GetChainDigestSkip2().IsDefault());
-            Assert.IsFalse(yotiAnchor.GetSubType().IsDefault());
-            Assert.IsFalse(yotiAnchor.GetOriginServerCerts().IsDefault());
-            Assert.IsFalse(yotiAnchor.GetValue().IsDefault());
+            Assert.AreEqual("", yotiAnchor.GetSubType());
+            Assert.AreEqual("DRIVING_LICENCE", yotiAnchor.GetValue());
+
+            X509Certificate2 firstOriginServerCert = yotiAnchor.GetOriginServerCerts().First();
+            Assert.AreEqual("CN=driving-licence-registration-server", firstOriginServerCert.Subject);
+            Assert.AreEqual("CN=driving-licence-registration-server", firstOriginServerCert.Issuer);
+            Assert.AreEqual("22B4AA0414D35D6C6019FE8EBD59B95C", firstOriginServerCert.SerialNumber);
+            Assert.AreEqual(new DateTime(2018, 4, 5, 14, 27, 36, DateTimeKind.Utc), firstOriginServerCert.NotBefore.ToUniversalTime());
+            Assert.AreEqual(new DateTime(2018, 4, 12, 14, 27, 36, DateTimeKind.Utc), firstOriginServerCert.NotAfter.ToUniversalTime());
+            Assert.AreEqual("3C753FFD1D8A359EC89AD2BD679563F2E4F9B767", firstOriginServerCert.Thumbprint);
         }
     }
 }
