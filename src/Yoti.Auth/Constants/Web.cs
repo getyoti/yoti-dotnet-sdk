f68dd59b53dfae03807b82a5dc847a7238361f72
echarrod <echarrod@users.noreply.github.com>
2019-08-22 15:26:31 +0100

    SDK-600: Add further validation, make Yoti host constants public

--- src/Yoti.Auth/Constants/Web.cs
+++ src/Yoti.Auth/Constants/Web.cs
@@ -1,14 +1,14 @@
 ﻿namespace Yoti.Auth.Constants
 {
-    internal static class Web
+    public static class Web
     {
-        private const string DefaultYotiHost = @"https://api.yoti.com";
+        public const string DefaultYotiHost = @"https://api.yoti.com";
         public const string YotiApiPathPrefix = "api/v1";
         public readonly static string DefaultYotiApiUrl = string.Join("/", DefaultYotiHost, YotiApiPathPrefix);
-        public const string AuthKeyHeader = "X-Yoti-Auth-Key";
-        public const string DigestHeader = "X-Yoti-Auth-Digest";
-        public const string YotiSdkHeader = "X-Yoti-SDK";
-        public const string YotiSdkVersionHeader = YotiSdkHeader + "-Version";
-        public const string SdkIdentifier = ".NET";
+        internal const string AuthKeyHeader = "X-Yoti-Auth-Key";
+        internal const string DigestHeader = "X-Yoti-Auth-Digest";
+        internal const string YotiSdkHeader = "X-Yoti-SDK";
+        internal const string YotiSdkVersionHeader = YotiSdkHeader + "-Version";
+        internal const string SdkIdentifier = ".NET";
     }
 }
--- test/Yoti.Auth.Tests/TestTools/DefaultValueComparer.cs
+++ test/Yoti.Auth.Tests/TestTools/DefaultValueComparer.cs
@@ -5,16 +5,5 @@ namespace Yoti.Auth.Tests.TestTools
 {
     internal static class DefaultValueComparer
     {
-        public static bool IsDefault(this object value)
-        {
-            if (value == null)
-                return true;
-
-            if (!value.GetType().GetTypeInfo().IsValueType)
-                return false;
-
-            object defaultValue = Activator.CreateInstance(value.GetType());
-            return value.Equals(defaultValue);
-        }
     }
 }
