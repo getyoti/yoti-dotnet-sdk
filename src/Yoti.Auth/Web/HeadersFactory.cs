c5fd50866b99da6663228d38b6564447888c4a3e
echarrod <echarrod@users.noreply.github.com>
2019-10-03 11:59:18 +0100

    SDK-1205: Update Request Builder to not include Auth Key Header by default

--- src/Yoti.Auth/Web/HeadersFactory.cs
+++ src/Yoti.Auth/Web/HeadersFactory.cs
@@ -8,16 +8,14 @@ namespace Yoti.Auth.Web
     {
         internal static HttpRequestMessage AddHeaders(HttpRequestMessage httpRequestMessage, AsymmetricCipherKeyPair keyPair, HttpMethod httpMethod, string endpoint, byte[] httpContent)
         {
-            string authKey = CryptoEngine.GetAuthKey(keyPair);
             string authDigest = SignedMessageFactory.SignMessage(httpMethod, endpoint, keyPair, httpContent);
             string SDKVersion = typeof(YotiClientEngine).GetTypeInfo().Assembly.GetName().Version.ToString();
 
-            return PutHeaders(httpRequestMessage, authDigest, authKey, SDKVersion);
+            return PutHeaders(httpRequestMessage, authDigest, SDKVersion);
         }
 
-        internal static HttpRequestMessage PutHeaders(HttpRequestMessage httpRequestMessage, string authDigest, string authKey, string SDKVersion)
+        internal static HttpRequestMessage PutHeaders(HttpRequestMessage httpRequestMessage, string authDigest, string SDKVersion)
         {
-            httpRequestMessage.Headers.Add(Constants.Web.AuthKeyHeader, authKey);
             httpRequestMessage.Headers.Add(Constants.Web.DigestHeader, authDigest);
             httpRequestMessage.Headers.Add(Constants.Web.YotiSdkHeader, Constants.Web.SdkIdentifier);
             httpRequestMessage.Headers.Add(Constants.Web.YotiSdkVersionHeader, $"{Constants.Web.SdkIdentifier}-{SDKVersion}");
