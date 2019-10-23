e321c62637698a7abefd34ddad674111e256cfa9
echarrod <echarrod@users.noreply.github.com>
2019-10-23 15:50:22 +0100

    SDK-976: Add sonarcloud scanning to pipeline, fix issues in 47Example project, add projectGuids to .NET Core projects

--- src/Examples/Profile/47Example/App_Start/RouteConfig.cs
+++ src/Examples/Profile/47Example/App_Start/RouteConfig.cs
@@ -3,7 +3,7 @@ using System.Web.Routing;
 
 namespace _47Example
 {
-    public class RouteConfig
+    public static class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
