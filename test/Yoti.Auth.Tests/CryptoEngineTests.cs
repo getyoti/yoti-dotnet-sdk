bb587809c2576463cec8fdaaa389dd75f603ef3a
echarrod <echarrod@gmail.com>
2020-01-20 14:39:51 +0000

    SDK-1386: Add validation and tests for one time use token varations

---
+++ test/Yoti.Auth.Tests/CryptoEngineTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yoti.Auth.Tests.Common;
+
+namespace Yoti.Auth.Tests
+{
+    [TestClass]
+    public class CryptoEngineTests
+    {
+        [TestMethod]
+        public void NonBase64TokenThrowsError()
+        {
+            var exception = Assert.ThrowsException<FormatException>(() =>
+            {
+                CryptoEngine.DecryptToken("_aasi", KeyPair.Get());
+            });
+
+            Assert.IsTrue(exception.Message.Contains("The input is not a valid Base-64 string as it contains a non-base 64 character"));
+        }
+
+        [TestMethod]
+        public void EmptyOneTimeUseTokenThrowsError()
+        {
+            var exception = Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                CryptoEngine.DecryptToken("", KeyPair.Get());
+            });
+
+            Assert.IsTrue(exception.Message.Contains("one time use token"));
+        }
+    }
+}
