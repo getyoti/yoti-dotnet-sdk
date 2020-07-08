07215a0930c5726f137484dfd21fc4c72c5b0c83
echarrod <echarrod@users.noreply.github.com>
2020-07-08 10:01:18 +0100

    Remove Sandbox tests from pre-commit hook after moving to own repo

--- test.sh
+++ test.sh
@@ -2,4 +2,3 @@
 
 set -e
 dotnet test test/Yoti.Auth.Tests/Yoti.Auth.Tests.csproj -c Release --verbosity minimal
-dotnet test test/Yoti.Auth.Sandbox.Tests/Yoti.Auth.Sandbox.Tests.csproj -c Release --verbosity minimal
