b687eeb3886aef30d450285eac03e27555799075
echarrod <echarrod@users.noreply.github.com>
2020-07-08 10:03:43 +0100

    SDK-1636: Add chipdata, DocumentIdPhoto

--- src/Yoti.Auth/DocScan/Session/Create/Task/RequestedTextExtractionTaskConfig.cs
+++ src/Yoti.Auth/DocScan/Session/Create/Task/RequestedTextExtractionTaskConfig.cs
@@ -4,12 +4,16 @@ namespace Yoti.Auth.DocScan.Session.Create.Task
 {
     public class RequestedTextExtractionTaskConfig : RequestedTaskConfig
     {
-        public RequestedTextExtractionTaskConfig(string manualCheck)
+        public RequestedTextExtractionTaskConfig(string manualCheck, string chipData = null)
         {
             ManualCheck = manualCheck;
+            ChipData = chipData;
         }
 
         [JsonProperty(PropertyName = "manual_check")]
         public string ManualCheck { get; }
+
+        [JsonProperty(PropertyName = "chip_data")]
+        public string ChipData { get; }
     }
 }
