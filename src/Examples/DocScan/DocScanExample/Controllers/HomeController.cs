4fc38773fc92aa94aefbbd6f93543ae392d620fd
echarrod <echarrod@users.noreply.github.com>
2020-09-11 16:24:19 +0100

    SDK-1748: Remove FaceMap from demo

--- src/Examples/DocScan/DocScanExample/Controllers/HomeController.cs
+++ src/Examples/DocScan/DocScanExample/Controllers/HomeController.cs
@@ -79,22 +79,11 @@ namespace DocScanExample.Controllers
         }
 
         [Route("media")]
-        public IActionResult Media(string mediaId, string sessionId, int? base64zip)
+        public IActionResult Media(string mediaId, string sessionId)
         {
             MediaValue media = _client.GetMediaContent(sessionId, mediaId);
 
-            var contentType = media.GetMIMEType();
-            var content = media.GetContent();
-
-            if (contentType == "application/octet-stream" && base64zip == 1)
-            {
-                string base64Media = Encoding.ASCII.GetString(content);
-                byte[] mediaBytes = Convert.FromBase64String(base64Media);
-
-                return File(mediaBytes, "application/zip", $"{mediaId}.zip");
-            }
-
-            return File(content, contentType);
+            return File(media.GetContent(), media.GetMIMEType());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
