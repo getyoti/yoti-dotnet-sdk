4f13a1b85742324d14d843b3c5cf436133a5aa0b
echarrod <echarrod@users.noreply.github.com>
2020-09-28 12:35:37 +0100

    SDK-1457: Add DocumentFilter tests

---
+++ test/Yoti.Auth.Tests/DocScan/Session/Create/Filter/DocumentRestrictionsBuilderTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yoti.Auth.DocScan.Session.Create.Filter;
+
+namespace Yoti.Auth.Tests.DocScan.Session.Create.Filter
+{
+    [TestClass]
+    public class DocumentRestrictionBuilderTests
+    {
+        [TestMethod]
+        public void ShouldBuildWithCountries()
+        {
+            DocumentRestriction result = new DocumentRestrictionBuilder()
+                .WithCountries(new List<string> { "CO1" })
+                .Build();
+
+            Assert.AreEqual("CO1", result.CountryCodes.Single());
+            Assert.IsNull(result.DocumentTypes);
+        }
+
+        [TestMethod]
+        public void ShouldBuildWithDocumentTypes()
+        {
+            DocumentRestriction result = new DocumentRestrictionBuilder()
+                .WithDocumentTypes(new List<string> { "PASSPORT" })
+                .Build();
+
+            Assert.AreEqual("PASSPORT", result.DocumentTypes.Single());
+            Assert.IsNull(result.CountryCodes);
+        }
+    }
+}
---
+++ test/Yoti.Auth.Tests/DocScan/Session/Create/Filter/OrthogonalRestrictionsFilterBuilderTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yoti.Auth.DocScan.Session.Create.Filter;
+
+namespace Yoti.Auth.Tests.DocScan.Session.Create.Filter
+{
+    [TestClass]
+    public class OrthogonalRestrictionsFilterBuilderTests
+    {
+        [TestMethod]
+        public void WithIncludedCountries()
+        {
+            OrthogonalRestrictionsFilter result = new OrthogonalRestrictionsFilterBuilder()
+                .WithIncludedCountries(new List<string> { "CO1" })
+                .Build();
+
+            Assert.AreEqual("ORTHOGONAL_RESTRICTIONS", result.Type);
+            Assert.AreEqual("CO1", result.CountryRestriction.CountryCodes.Single());
+            Assert.AreEqual("WHITELIST", result.CountryRestriction.Inclusion);
+            Assert.IsNull(result.TypeRestriction);
+        }
+
+        [TestMethod]
+        public void WithExcludedCountries()
+        {
+            OrthogonalRestrictionsFilter result = new OrthogonalRestrictionsFilterBuilder()
+                .WithExcludedCountries(new List<string> { "CO2" })
+                .Build();
+
+            Assert.AreEqual("CO2", result.CountryRestriction.CountryCodes.Single());
+            Assert.AreEqual("BLACKLIST", result.CountryRestriction.Inclusion);
+            Assert.IsNull(result.TypeRestriction);
+        }
+
+        [TestMethod]
+        public void WithIncludedDocumentTypes()
+        {
+            OrthogonalRestrictionsFilter result = new OrthogonalRestrictionsFilterBuilder()
+                .WithIncludedDocumentTypes(new List<string> { "CO3" })
+                .Build();
+
+            Assert.AreEqual("ORTHOGONAL_RESTRICTIONS", result.Type);
+            Assert.AreEqual("CO3", result.TypeRestriction.DocumentTypes.Single());
+            Assert.AreEqual("WHITELIST", result.TypeRestriction.Inclusion);
+            Assert.IsNull(result.CountryRestriction);
+        }
+
+        [TestMethod]
+        public void WithExcludedDocumentTypes()
+        {
+            OrthogonalRestrictionsFilter result = new OrthogonalRestrictionsFilterBuilder()
+                .WithExcludedDocumentTypes(new List<string> { "CO4" })
+                .Build();
+
+            Assert.AreEqual("ORTHOGONAL_RESTRICTIONS", result.Type);
+            Assert.AreEqual("CO4", result.TypeRestriction.DocumentTypes.Single());
+            Assert.AreEqual("BLACKLIST", result.TypeRestriction.Inclusion);
+            Assert.IsNull(result.CountryRestriction);
+        }
+    }
+}
