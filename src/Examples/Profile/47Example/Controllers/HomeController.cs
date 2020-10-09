af8b759393f047af50db5b17f2dde1e49e862d4b
echarrod <echarrod@users.noreply.github.com>
2020-10-09 11:13:06 +0100

    NA: Remove unused usings

--- src/Examples/Profile/47Example/Controllers/HomeController.cs
+++ src/Examples/Profile/47Example/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Web.Mvc;
 using Yoti.Auth;
 using Yoti.Auth.ShareUrl;
-using Yoti.Auth.ShareUrl.Extensions;
 using Yoti.Auth.ShareUrl.Policy;
 
 namespace _47Example.Controllers
