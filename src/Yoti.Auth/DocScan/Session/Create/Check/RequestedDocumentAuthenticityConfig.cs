e51106e5a574cc6c1706085ee38d2f8379a63a20
echarrod <echarrod@users.noreply.github.com>
2020-10-09 12:03:50 +0100

    SDK-1731: Add ManualFallback option to Document Authenticity Check

--- src/Yoti.Auth/DocScan/Session/Create/Check/RequestedDocumentAuthenticityConfig.cs
+++ src/Yoti.Auth/DocScan/Session/Create/Check/RequestedDocumentAuthenticityConfig.cs
@@ -1,9 +1,15 @@
-﻿namespace Yoti.Auth.DocScan.Session.Create.Check
+﻿using Newtonsoft.Json;
+
+namespace Yoti.Auth.DocScan.Session.Create.Check
 {
     public class RequestedDocumentAuthenticityConfig : RequestedCheckConfig
     {
-        public RequestedDocumentAuthenticityConfig()
+        [JsonProperty(PropertyName = "manual_check")]
+        public string ManualCheck { get; }
+
+        public RequestedDocumentAuthenticityConfig(string manualCheck)
         {
+            ManualCheck = manualCheck;
         }
     }
 }
