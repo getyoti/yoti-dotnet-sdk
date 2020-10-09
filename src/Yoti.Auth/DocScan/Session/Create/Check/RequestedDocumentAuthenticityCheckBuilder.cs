f1c2c0afa66e980ab9854093ac8acf5dc9f85a2f
echarrod <echarrod@users.noreply.github.com>
2020-10-09 12:03:50 +0100

    SDK-1731: Add ManualFallback option to Document Authenticity Check

--- src/Yoti.Auth/DocScan/Session/Create/Check/RequestedDocumentAuthenticityCheckBuilder.cs
+++ src/Yoti.Auth/DocScan/Session/Create/Check/RequestedDocumentAuthenticityCheckBuilder.cs
@@ -2,9 +2,41 @@
 {
     public class RequestedDocumentAuthenticityCheckBuilder
     {
+        private string _manualCheck;
+
+        /// <summary>
+        /// Requires that a manual follow-up check is always performed
+        /// </summary>
+        /// <returns>The builder</returns>
+        public RequestedDocumentAuthenticityCheckBuilder WithManualCheckAlways()
+        {
+            _manualCheck = Constants.DocScanConstants.Always;
+            return this;
+        }
+
+        /// <summary>
+        /// Requires that a manual follow-up check is performed only on failed Checks, and those with a low level of confidence
+        /// </summary>
+        /// <returns>The builder</returns>
+        public RequestedDocumentAuthenticityCheckBuilder WithManualCheckFallback()
+        {
+            _manualCheck = Constants.DocScanConstants.Fallback;
+            return this;
+        }
+
+        /// <summary>
+        /// Requires that only an automated Check is performed.  No manual follow-up Check will ever be initiated
+        /// </summary>
+        /// <returns>The builder</returns>
+        public RequestedDocumentAuthenticityCheckBuilder WithManualCheckNever()
+        {
+            _manualCheck = Constants.DocScanConstants.Never;
+            return this;
+        }
+
         public RequestedDocumentAuthenticityCheck Build()
         {
-            return new RequestedDocumentAuthenticityCheck(new RequestedDocumentAuthenticityConfig());
+            return new RequestedDocumentAuthenticityCheck(new RequestedDocumentAuthenticityConfig(_manualCheck));
         }
     }
 }
--- src/Yoti.Auth/DocScan/Session/Create/Check/RequestedFaceMatchCheckBuilder.cs
+++ src/Yoti.Auth/DocScan/Session/Create/Check/RequestedFaceMatchCheckBuilder.cs
@@ -6,18 +6,30 @@ namespace Yoti.Auth.DocScan.Session.Create.Check
     {
         private string _manualCheck;
 
+        /// <summary>
+        /// Requires that a manual follow-up check is always performed
+        /// </summary>
+        /// <returns>The builder</returns>
         public RequestedFaceMatchCheckBuilder WithManualCheckAlways()
         {
             _manualCheck = DocScanConstants.Always;
             return this;
         }
 
+        /// <summary>
+        /// Requires that a manual follow-up check is performed only on failed Checks, and those with a low level of confidence
+        /// </summary>
+        /// <returns>The builder</returns>
         public RequestedFaceMatchCheckBuilder WithManualCheckFallback()
         {
             _manualCheck = DocScanConstants.Fallback;
             return this;
         }
 
+        /// <summary>
+        /// Requires that only an automated Check is performed.  No manual follow-up Check will ever be initiated
+        /// </summary>
+        /// <returns>The builder</returns>
         public RequestedFaceMatchCheckBuilder WithManualCheckNever()
         {
             _manualCheck = DocScanConstants.Never;
