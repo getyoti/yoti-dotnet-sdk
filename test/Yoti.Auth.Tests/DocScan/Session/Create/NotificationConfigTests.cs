a7906a596aac29a720db8473fab257dc584de6e5
BenSmithYoti <ben.smith@yoti.com>
2021-10-19 14:07:37 +0100

    SDK-1842: Add AuthType to IDV (Doc Scan)
    The Identity verification service can accept a value for configuring the
    Authentication Scheme to use in HTTP Authorization Headers for
    callback messages to Relying Businesses' systems. The authentication
    scheme can be set to either Basic or Bearer. This will be included with
    any authorization token set in the notification configuration. This can
    be used to help secure callback endpoints and so provide some level of
    certainty that notifications originated with Yoti systems.

---
+++ test/Yoti.Auth.Tests/DocScan/Session/Create/NotificationConfigTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yoti.Auth.DocScan.Session.Create;
+
+namespace Yoti.Auth.Tests.DocScan.Session.Create
+{
+    [TestClass]
+    public class NotificationConfigTests
+    {
+        [TestMethod]
+        public void ShouldBeConstructableWithoutAuthType()
+        {
+            string authToken = "someAuthToken"; 
+            string endpoint = "someEndpoint";
+            string topicName = "someTopic";
+            List<string> topics = new List<string> { topicName };
+
+            NotificationConfig notificationConfig =
+              new NotificationConfig(authToken, endpoint, topics);
+
+            Assert.AreEqual(authToken, notificationConfig.AuthToken);
+            Assert.AreEqual(endpoint, notificationConfig.Endpoint);
+            Assert.AreEqual(topicName, notificationConfig.Topics.Single());
+        }
+
+        [TestMethod]
+        public void ShouldBeConstructableWithAuthType()
+        {
+            string authToken = "someAuthToken";
+            string endpoint = "someEndpoint";
+            string topicName = "someTopic";
+            List<string> topics = new List<string>{ topicName };
+            string authType = "BASIC";
+
+            NotificationConfig notificationConfig =
+              new NotificationConfig(authToken, endpoint, topics, authType);
+
+            Assert.AreEqual(authToken, notificationConfig.AuthToken);
+            Assert.AreEqual(endpoint, notificationConfig.Endpoint);
+            Assert.AreEqual(topicName, notificationConfig.Topics.Single());
+            Assert.AreEqual(authType, notificationConfig.AuthType);
+        }
+    }
+}
