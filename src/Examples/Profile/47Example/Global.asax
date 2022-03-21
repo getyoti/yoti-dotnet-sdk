fe76932fcb1ac22a1731b6c618153e84159090e3
ben.smith <ben.smith@yoti.com>
2022-03-21 16:29:09 +0000

    Remove 47Example project folder and files from SC.

--- src/Examples/Profile/47Example/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_47Example.MvcApplication" Language="C#" %>
--- src/Examples/Profile/47Example/GlobalSuppressions.cs
+++
@@ -1,7 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "We want to show all errors here to aid debugging", Scope = "member", Target = "~M:_47Example.Controllers.AccountController.Connect(System.String)~System.Web.Mvc.ActionResult")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "We want to show all errors here to aid debugging", Scope = "member", Target = "~M:_47Example.Controllers.HomeController.DynamicScenario~System.Web.Mvc.ActionResult")]
