05949868fd85de4dc061b9b9613d443ce64df05c
Ed H <echarrod@users.noreply.github.com>
2022-03-29 17:25:05 +0100

    NA: Fix Sonar code smell
    > SonarQube: Loops should be simplified with "LINQ" expressions
    > When a loop is filtering, selecting or aggregating, those functions can be handled with a clearer, more concise LINQ expression instead.

--- src/Yoti.Auth/Profile/BaseProfile.cs
+++ src/Yoti.Auth/Profile/BaseProfile.cs
@@ -150,24 +150,14 @@ namespace Yoti.Auth.Profile
         {
             if (prefix == null)
                 throw new ArgumentNullException(nameof(prefix));
-
-            List<YotiAttribute<T>> matches = new List<YotiAttribute<T>>();
-
-            foreach (KeyValuePair<string, List<BaseAttribute>> attributesByName in _attributes)
-            {
-                if (attributesByName.Key.StartsWith(prefix, StringComparison.Ordinal))
-                {
-                    foreach (var attribute in attributesByName.Value)
-                    {
-                        if (attribute is YotiAttribute<T> castableAttribute)
-                        {
-                            matches.Add(castableAttribute);
-                        }
-                    }
-                }
-            }
-
-            return matches;
+            return 
+                _attributes.Where(
+                    attributesByName => attributesByName.Key.StartsWith(
+                        prefix, StringComparison.Ordinal))
+                .SelectMany(
+                    attributesByName => attributesByName.Value.Select(
+                    attribute => attribute as YotiAttribute<T>))
+                .ToList();
         }
     }
 }
