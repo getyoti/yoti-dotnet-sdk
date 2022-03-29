fb73d509dd0453c4200a7d5bd35cc0b9d62bc94d
Ed H <echarrod@users.noreply.github.com>
2022-03-29 16:47:27 +0100

    Add IDV Identity Profile

---
+++ src/Yoti.Auth/DocScan/Session/Retrieve/IdentityProfile/FailureReasonResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Yoti.Auth.DocScan.Session.Retrieve.IdentityProfile
+{
+    public class FailureReasonResponse
+    {
+        [JsonProperty(PropertyName = "reason_code")]
+        public  string ReasonCode { get; private set; }
+    }
+}
