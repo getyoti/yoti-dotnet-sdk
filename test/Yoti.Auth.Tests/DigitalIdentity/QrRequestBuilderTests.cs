2ea167a382968d043f03bd2de2b879f4d21302c0
mehmet-yoti <111424390+mehmet-yoti@users.noreply.github.com>
2024-01-10 15:05:38 +0300

    Sdk 2252 net create qr code (#441)
    * SDK-2252:added net create qr code
    * SDK-2252:Updated validation rule
    * SDK-2252:removed net 2.1 support for security issues

---
+++ test/Yoti.Auth.Tests/DigitalIdentity/QrRequestBuilderTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yoti.Auth.DigitalIdentity;
+using Yoti.Auth.Tests.TestData;
+
+namespace Yoti.Auth.Tests.DigitalIdentity
+{
+    [TestClass]
+    public class QrRequestBuilderTests
+    {
+
+        private const string _someTransportString = "someTransport";
+        private const string _someDisplayMode = "someDisplay";
+
+
+
+        [TestMethod]
+        public void ShouldBuildADynamicScenario()
+        {
+            QrRequest result = new QrRequestBuilder()
+                .WithDisplayMode(_someDisplayMode)
+                .WithTransport(_someTransportString)
+               .Build();
+
+            
+            Assert.AreEqual(_someDisplayMode, result.DisplayMode);
+            Assert.AreEqual(_someTransportString, result.Transport);
+        }
+        
+    }
+}
---
+++ test/Yoti.Auth.Tests/TestTools/CreateQr.cs
@@ -0,0 +1,15 @@
+﻿using Yoti.Auth.ShareUrl;
+using Yoti.Auth.DigitalIdentity.Policy;
+using Yoti.Auth.Tests.TestData;
+using Yoti.Auth.DigitalIdentity;
+
+namespace Yoti.Auth.Tests.TestTools
+{
+    internal static class CreateQr
+    {
+        public static QrRequest CreateQrStandard()
+        {
+            return new QrRequest();
+        }
+    }
+}
