784245fc5302bd229ff1fd16c04c95d3120e3771
mehmet-yoti <mehmet.sepici@yoti.com>
2024-03-20 11:54:03 +0000

    SDK-2264:Added digitalidentity project, controllers

---
+++ src/Examples/DigitalIdentity/DigitalIdentity/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Auto-generated", Scope = "member", Target = "~M:DigitalIdentityExample.Startup.ConfigureServices(Microsoft.Extensions.DependencyInjection.IServiceCollection)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Auto-generated", Scope = "member", Target = "~M:DigitalIdentityExample.Startup.Configure(Microsoft.AspNetCore.Builder.IApplicationBuilder,Microsoft.AspNetCore.Hosting.IHostingEnvironment)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "We want to show all errors here to aid debugging", Scope = "member", Target = "~M:DigitalIdentityExample.Controllers.AccountController.Connect(System.String)~Microsoft.AspNetCore.Mvc.ActionResult")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "We want to show all errors here to aid debugging", Scope = "member", Target = "~M:DigitalIdentityExample.Controllers.HomeController.DynamicScenario~Microsoft.AspNetCore.Mvc.IActionResult")]
---
+++ src/Examples/DigitalIdentity/DigitalIdentity/README.md
@@ -0,0 +1,21 @@
+# .NET Core Example project 
+
+## 1) Setup
+1) Clone this repo
+1) Navigate to this folder
+1) Rename the [.env.example](.env.example) file to `.env`
+1) Fill in the environment variables in this file with the ones specific to your application, generated in the [Yoti Hub](https://hub.yoti.com) when you create (and then publish) your application
+
+## 2a) Running With Docker
+1) From the Yoti Hub, set the application domain to `localhost:44380` and the scenario callback URL to `/account/connect`
+1) `docker-compose build --no-cache`
+1) `docker-compose up`
+1) Navigate to <https://localhost:44380>
+
+>If you encounter a "permission denied" error when trying to access the mounted .pem file, try disabling and re-enabling your shared drive in Docker settings.
+
+## 2b) Running With .NET Core installed locally
+1) From the Yoti Hub, set the application domain to `localhost:44344` and the scenario callback URL to `/account/connect`
+1) Download the .NET SDK for your operating system from step no.1 on ([Windows](https://www.microsoft.com/net/learn/get-started/windows) | [Linux](https://www.microsoft.com/net/learn/get-started/linux/rhel) | [MacOS](https://www.microsoft.com/net/learn/get-started/macos))
+1) Enter `dotnet run -p CoreExample.csproj` into the terminal 
+1) Navigate to the page specified in the terminal window, which should be <https://localhost:44344>
