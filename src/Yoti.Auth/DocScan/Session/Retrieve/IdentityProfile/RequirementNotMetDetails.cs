693d271ea0f680c57bf67ad66d5ae7e254adeeb6
mehmet-yoti <mehmet.sepici@yoti.com>
2024-06-25 22:11:14 +0100

    SDK-2374 added failure reason info to idv and added tests

---
+++ src/Yoti.Auth/DocScan/Session/Retrieve/IdentityProfile/RequirementNotMetDetails.cs
@@ -0,0 +1,22 @@
+using Newtonsoft.Json;
+
+namespace Yoti.Auth.DocScan.Session.Retrieve.IdentityProfile;
+
+public class RequirementNotMetDetails
+{
+    [JsonProperty(PropertyName = "failure_type")]
+    public string FailureType { get; private set; }
+    [JsonProperty(PropertyName = "document_type")]
+    public string DocumentType { get; private set; }
+    [JsonProperty(PropertyName = "document_country_iso_code")]
+    public string DocumentCountryIsoCode { get; private set; }
+    [JsonProperty(PropertyName = "audit_id")]
+    public string AuditId { get; private set; }
+    [JsonProperty(PropertyName = "details")]
+    public string Details { get; private set; }
+    public string GetFailureType() => FailureType;
+    public string GetDetails() => Details;
+    public string GetAuditId() => AuditId;
+    public string GetDocumentCountryIsoCode() => DocumentCountryIsoCode;
+    public string GetDocumentType() => DocumentType;
+}
