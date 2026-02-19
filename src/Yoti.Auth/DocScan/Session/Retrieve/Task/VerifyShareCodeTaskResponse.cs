2a18134124c4808bd7f0dc0fc089b54e029a5b3c
mehmet-yoti <mehmet.sepici@yoti.com>
2026-02-19 14:02:12 +0000

    SDK-2753 added support for requesting and retrieving share code resource tasks

---
+++ src/Yoti.Auth/DocScan/Session/Retrieve/Task/VerifyShareCodeTaskResponse.cs
@@ -0,0 +1,9 @@
+namespace Yoti.Auth.DocScan.Session.Retrieve.Task
+{
+    /// <summary>
+    /// Represents a Verify Share Code task response
+    /// </summary>
+    public class VerifyShareCodeTaskResponse : TaskResponse
+    {
+    }
+}
