8f26842a4979aab4878c9f414c237dab2aa9177c
mehmet-yoti <mehmet.sepici@yoti.com>
2026-03-06 15:56:07 +0000

    SDK-2300 Add applicant profile deserialization support for GET sessions response, and updated success page on docscan example

--- src/Yoti.Auth/DocScan/Session/Retrieve/Resource/ApplicantProfileResourceResponse.cs
+++ src/Yoti.Auth/DocScan/Session/Retrieve/Resource/ApplicantProfileResourceResponse.cs
@@ -1,3 +1,4 @@
+using System;
 using Newtonsoft.Json;
 
 namespace Yoti.Auth.DocScan.Session.Retrieve.Resource
@@ -9,5 +10,11 @@ namespace Yoti.Auth.DocScan.Session.Retrieve.Resource
     {
         [JsonProperty(PropertyName = "media")]
         public MediaResponse Media { get; private set; }
+
+        [JsonProperty(PropertyName = "created_at")]
+        public DateTime? CreatedAt { get; private set; }
+
+        [JsonProperty(PropertyName = "last_updated")]
+        public DateTime? LastUpdated { get; private set; }
     }
 }
